daf4e178f812cc898962360d9a8cd4888eab7f69
Shruti Verma <cshruti369@gmail.com>
2020-11-17 16:02:11 +0530

    Week 4

--- mod-4/SpeakGoodbye.js
+++ mod-4/SpeakGoodbye.js
@@ -3,6 +3,6 @@
     var speakWord = "Good Bye";
     byeSpeaker.speak = function speak(name) {
         console.log(speakWord + " " + name);
-    };
+    }
     window.byeSpeaker = byeSpeaker;
 })(window);
